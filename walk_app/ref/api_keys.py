d923bb74d623892ea9fa841ded5e65ead0493c14
[froebbm] <67332792+BFroebRPG@users.noreply.github.com>
2025-04-03 01:14:48 -0400

    removing keys

---
+++ walk_app/ref/api_keys.py
@@ -0,0 +1,4 @@
+REFRESH_KEY = "AMf-vBzR-xGRX_Pxj2m-3T5u9jvRoqOQkCyUzIYGagb9PqpbWlzefa0uEUQhyYv"\
+    "EchBAwkdQoBU4l1A1rht3Y7FevM9JnzqgL7_BjM57edAUXikrIkl3wW_iepEMTFQqu38Njdi"\
+        "Yip1PJniSTa67ZdjIfpTijklyX0xi1dD1wSlBApE3IVo9VBiv0cjgrjCZNcrlqRqmax48"\
+            "P0FJoefMZf5h9einbh5GDWT-1mtxSCQhrlzV46e4t9M"
