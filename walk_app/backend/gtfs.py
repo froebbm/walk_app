4d166c20de790f539251403ea47b3396811a9079
[froebbm] <67332792+BFroebRPG@users.noreply.github.com>
2025-04-05 00:16:56 -0400

    convert text files to geodataframes in gtfs class

--- walk_app/backend/gtfs.py
+++ walk_app/backend/gtfs.py
@@ -1,7 +1,25 @@
 import requests
 import pandas as pd
+import geopandas as gpd
+
 from zipfile import ZipFile
 from io import BytesIO
+from shapely import Point, LineString
+
+def convert_xy_to_point(df, lat="stop_lat", lon="stop_lon"):
+    pt_geo = [Point(xy) for xy in zip(df[lon],df[lat])]
+    pts = gpd.GeoDataFrame(df, geometry=pt_geo, crs="wgs84")
+    return pts
+
+def convert_shapes_to_gdf(df, lon='shape_pt_lon', lat='shape_pt_lat'):
+    pts = convert_xy_to_point(df, lon=lon, lat=lat)
+    
+    lines = pts.groupby('shape_id')['geometry'].apply(
+        lambda x: LineString(x.tolist())
+        )
+    
+    lines = gpd.GeoDataFrame(lines, geometry="geometry", crs="wgs84")
+    return lines
 
 class gtfs:
     def __init__(self, shapes, trips, stops, routes):
@@ -15,10 +33,14 @@ class gtfs:
         r = requests.get(path)
         z = ZipFile(BytesIO(r.content))
 
-        shapes = pd.read_csv(z.open("shapes.txt"))
-        trips = pd.read_csv(z.open("trips.txt"))
-        stops = pd.read_csv(z.open("stops.txt"))
         routes = pd.read_csv(z.open("routes.txt"))
+        trips = pd.read_csv(z.open("trips.txt"))
+        
+        shapes = pd.read_csv(z.open("shapes.txt"))
+        shapes = convert_shapes_to_gdf(shapes)
+        
+        stops = pd.read_csv(z.open("stops.txt"))
+        stops = convert_xy_to_point(stops)
         
         return cls(
             shapes=shapes,
